6293b9281a7c37849cc53d9369614f668f1c4337
DonaldoAr <jefe.ceo@gmail.com>
2021-08-19 19:23:41 -0500

    Add a  struture for cosoles menu

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+//const Tarea = require('../models/tarea');
 require('colors');
 
 const menuOpts = [
@@ -9,32 +10,16 @@ const menuOpts = [
         //choices: ['opt1', 'opt2', 'opt3']
         choices: [
             {
-                value: '1',
-                name: '1. Crear tarea'
+                value: 1,
+                name: '1'.green+'. Buscar ciudad'
             },
             {
-                value: '2',
-                name: '2. Listar tareas'
+                value: 2,
+                name: '2'.red+'. Historial'
             },
             {
-                value: '3',
-                name: '3. Listar tareas completadas'
-            },
-            {
-                value: '4',
-                name: '4. Listar tareas pendientes'
-            },
-            {
-                value: '5',
-                name: '5. Completar tarea(s)'
-            },
-            {
-                value: '6',
-                name: '6. Borrar tarea'
-            },
-            {
-                value: '0',
-                name: '0. Salir'
+                value: 0,
+                name: '0'.red+'. Salir'
             },
         ]
     }
@@ -43,7 +28,7 @@ const menuOpts = [
 const inquirerMenu = async()=>{
     console.clear();
     console.log('======================================='.red);
-    console.log(' Seleccione una opción '.red);
+    console.log(' Seleccione una opción '.white);
     console.log('======================================='.red);
 
     const { option } = await inquirer.prompt(menuOpts);
@@ -65,7 +50,99 @@ const pause = async () =>{
     return input;
 }
 
+const leerInput = async( message )=>{
+    const question =[
+        {
+            type: 'input',
+            name: 'desc',
+            message,
+            validate( value ){ // We should write the data
+                if(value.length === 0){
+                    return 'Por favor ingrese un valor';
+                }
+                return true;
+            }
+        }
+    ];
+    
+    const { desc } = await inquirer.prompt(question);
+    return desc;
+}
+
+const listadoTareasBorrar = async(tareas = []) => {
+
+    const choices = tareas.map( (tarea, i) =>{
+        const idx = `${i + 1}.`.green;
+        return{
+            value: tarea.id,
+            name: `${idx}.- ${tarea.desc}`
+        }
+    });
+
+    choices.unshift({
+        value: '0',
+        name: '0.'.green + ' Cancelar'
+    })
+
+    const preguntas = [
+        {
+            type: 'list',
+            name: 'id',
+            message: 'Borrar',
+            choices
+        }
+    ]
+
+    const { id } = await inquirer.prompt( preguntas );
+
+    return id;
+}
+
+const confirmar = async( message ) => {
+    const question = [
+        {
+            type: 'confirm',
+            name: 'ok',
+            message
+        }
+    ];
+    const { ok } = await inquirer.prompt( question );
+    return ok;
+}
+
+const mostrarListadoChecklist = async(tareas = []) => {
+
+    const choices = tareas.map( (tarea, i) =>{
+        const idx = `${i + 1}.`.green;
+        return{
+            value: tarea.id,
+            name: `${idx}.- ${tarea.desc}`,
+            //checked: true
+            checked: (tarea.completadoEn)? true : false
+        }
+    });
+
+    const pregunta = [
+        {
+            type: 'checkbox',
+            name: 'ids',
+            message: 'Seleccione',
+            choices
+        }
+    ]
+
+    const { ids } = await inquirer.prompt( pregunta );
+
+    return ids;
+}
+
+
+
 module.exports={
     inquirerMenu,
-    pause
+    pause,
+    leerInput,
+    //listadoTareasBorrar,
+    //confirmar,
+    //mostrarListadoChecklist
 }
--- index.js
+++ index.js
@@ -1,8 +1,34 @@
+const { leerInput,
+    inquirerMenu,
+    pause } = require('./helpers/inquirer');
+
+require('colors');
 
 
-
-const main = async ()=>{
-    console.log("Hola mundo");
+const main = async () => {
+    let opt;
+    
+    do {
+        opt = await inquirerMenu();
+        switch ( opt ) {
+            
+            
+            case 1:
+                console.log('Hola');
+                await pause();
+                break;
+            case 2:
+                console.log('Hola2');
+                await pause();
+                break;
+            case 0:
+        
+                break;
+                
+        }
+    } while (opt !== 0);
+    //await pause();
 }
+    
 
 main();
