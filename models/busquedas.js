57b5d095a2b74ae63e10df783307f952d404c447
DonaldoAr <jefe.ceo@gmail.com>
2021-08-21 16:35:36 -0500

    Add a function asyn ciudad for shared a city

--- models/busquedas.js
+++ models/busquedas.js
@@ -2,18 +2,28 @@ const axios = require('axios').default;
 
 class Busquedas{
     historial = ['Tegucigalpa', 'Madril', 'San Jose'];
-
     constructor(){
         // TODO: Leer db si existe
     }
 
-    async ciudad(lugar = ''){
-        
+    get paramsMapbox(){
+        return {
+            'access_token': 'pk.eyJ1IjoiZG9uYWxkbzciLCJhIjoiY2tzbDJ0cmxyMGZyeTJ2dGVpcWlxMGFnaCJ9.4LjFZLEiaUhrOeJJ8gbrDQ',
+            'limit': 5,
+            'language': 'es'
+        }
+    }
 
+    async ciudad(lugar = ''){
         try{
             // Peticion http
-            //console.log('ciudad ',lugar);
-            const resp = await axios.get('https://reqres.in/api/users?page=2');
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ lugar }.json`,
+                params: this.paramsMapbox
+            })
+
+            const resp = await instance.get();
+            //const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Que.json?access_token=pk.eyJ1IjoiZG9uYWxkbzciLCJhIjoiY2tzbDJ0cmxyMGZyeTJ2dGVpcWlxMGFnaCJ9.4LjFZLEiaUhrOeJJ8gbrDQ&limit=5&language=es');
             console.log(resp.data);
             return []; // Retornar las lugares que coincidan
         }catch(e){
